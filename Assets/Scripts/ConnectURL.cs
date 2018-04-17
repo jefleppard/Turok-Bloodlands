d10bc6af79e9fa74460a3efea661a91ca3bc2175
Jeff Sparks <jeff-sparks@hotmail.com>
2018-04-17 12:15:15 -0700

    I made a bunch of crappy scripts!
    Started point manager & perhaps mixer connectivity stuff

---
+++ Assets/Scripts/ConnectURL.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectURL : MonoBehaviour {
+
+    void Start() {
+		print ("I'm just the dumb connect script. There's really nothing for me to do yet.");
+    }
+	
+	public void OpenURL (){
+		Application.OpenURL("http://unity3d.com/");
+	}
+}
