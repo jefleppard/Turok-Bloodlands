1d26475c467d5dd007a96dd8ff7000fcf8b77a8f
Chris Coniglio <chris.j.coniglio@gmail.com>
2018-04-17 09:43:16 -0700

    I added a socket thing

---
+++ Assets/Scripts/Socket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace Socket
+{
+    public class Sock
+    {
+        public static void Main(string[] args)
+        {
+            using (var ws = new WebSocket("ws://dragonsnest.far/Laputa"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine("Laputa says: " + e.Data);
+
+                ws.Connect();
+                ws.Send("BALUS");
+                // Console.ReadKey(true);
+            }
+        }
+    }
+}
