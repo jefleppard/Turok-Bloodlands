6210f6e1701a9eb95966cb19ecd29c8a015484fa
Chris Coniglio <chris.j.coniglio@gmail.com>
2018-04-17 21:40:48 -0700

    trying to connect

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -1,21 +1,33 @@
-﻿using System;
+﻿using UnityEngine;
+using System.Collections;
 using WebSocketSharp;
 
-namespace Socket
-{
-    public class Sock
-    {
-        public static void Main(string[] args)
-        {
-            using (var ws = new WebSocket("ws://dragonsnest.far/Laputa"))
-            {
-                ws.OnMessage += (sender, e) =>
-                    Console.WriteLine("Laputa says: " + e.Data);
 
-                ws.Connect();
-                ws.Send("BALUS");
-                // Console.ReadKey(true);
-            }
-        }
+
+public class Socket : MonoBehaviour
+{
+    private WebSocket socket;
+    void Start () {
+        //using (var socket = new WebSocket("ws://138.68.60.143:80"))
+
+        print("creating the socket connection");
+
+        socket = new WebSocket("ws://138.68.60.143:80");
+
+        //socket.Log.Level = LogLevel.Debug;
+
+        socket.OnOpen += (sender, e) =>
+            print("connected to the server");
+
+        socket.OnError += (sender, e) =>
+            print("error connecting " + e);
+
+        socket.OnMessage += (sender, e) =>
+            print("we got some data" + e.Data);
+
+        socket.OnClose += (sender, e) =>
+            print("connection closed" + e.Reason);
+
+        socket.ConnectAsync();
     }
 }
